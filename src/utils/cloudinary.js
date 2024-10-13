a2cbfdb85a807920fade03a4d1826218690bf39f
Akash-node <aakashnegi645@gmail.com>
2024-10-13 22:57:58 +0530

    packages added

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,15 +2,14 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({ 
-    cloud_name: Process.env.CLOUDINARY_NAME,
-    api_key: Process.env.CLOUDINARY_API_KEY, 
-    api_secret: Process.env.CLOUDINARY_API_SECERT // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECERT
 });
 
 const uploadOnCloudnary = async (localFilePath) => {
     try {
         if(!localFilePath) return null;
-
         //upload the file on cloudinary
         const response = cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
@@ -19,7 +18,6 @@ const uploadOnCloudnary = async (localFilePath) => {
         //file has been uploaded successfully
         console.log("file is uploaded succesfully", response.url);
         return response;
-        
         
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved tempopary file
